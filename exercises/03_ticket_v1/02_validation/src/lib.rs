38b28572f784374844b668b8008ae2fe174fb75a
Sergio De León <sergio.deleon@ubi-lab.com>
2024-05-22 17:25:09 +0900

    Learning about scopes and ownership

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -1,3 +1,5 @@
+use std::thread::panicking;
+
 struct Ticket {
     title: String,
     description: String,
@@ -18,7 +20,21 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if title == "" {
+            panic!("Title cannot be empty");
+        }
+        if description == "" {
+            panic!("Description cannot be empty");
+        }
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+        if title.as_bytes().len() > 50 {
+            panic!("Title cannot be longer than 50 characters");
+        }
+        if description.as_bytes().len() > 500 {
+            panic!("Description cannot be longer than 500 characters");
+        }
         Self {
             title,
             description,
