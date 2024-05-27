e22f813584e2600a5b97ff1a59ce6d70b01cc135
Sergio De León <sergio.deleon@ubi-lab.com>
2024-05-27 20:33:52 +0900

    Finished traits and rebased from upstream

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,78 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+use std::ops::{Add, Deref};
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self { value: value.into() }
+    }
+}
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        Self { value: *value }
+    }
+}
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        Self { value: (*value).into() }
+    }
+}
+impl Add for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value)
+        }
+    }
+}
+impl Add for &SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value)
+        }
+    }
+}
+impl Add<u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs)
+        }
+    }
+}
+impl Add<&u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(*rhs)
+        }
+    }
+}
+impl Deref for SaturatingU16 {
+    type Target = u16;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct SaturatingU16 {
+    value: u16,
+}
