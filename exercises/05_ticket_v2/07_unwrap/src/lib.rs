f84fec1dc3f1ea80e49de073beb37d6460730710
Sergio De León <sergio.deleon@ubi-lab.com>
2024-07-17 16:14:56 +0900

    Learned to return Results and unwrap Errors

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,20 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(err) => {
+            let err_start = &err[0..5];
+            if err_start == "Title" {
+                panic!("{}", &err)
+            }
+            Ticket {
+                title,
+                description: "Description not provided".to_string(),
+                status
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
